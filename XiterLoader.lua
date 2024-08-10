ed382b73fc99736116518ac313c0b21fd5f5fd3d
TwoXiter <xiterdev7@gmail.com>
2024-08-10 08:52:55 -0300

    Update XiterLoader.lua

--- XiterLoader.lua
+++ XiterLoader.lua
@@ -1,10 +1,25 @@
 local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
 MenuUI:WindowCreate("Xiter Hub","","dsc.gg/xiterhub")
-MenuUI:ButtonAdd("Blox Fruits",function()
+MenuUI:ButtonAdd("Blox Fruits Main",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/BloxFruits.lua"))()
+    MenuUI:WindowDelete()
+end)
+MenuUI:ButtonAdd("Blox Fruits Kaitun(beta)", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/Kaitun.lua"))()
     MenuUI:WindowDelete()
 end)
 MenuUI:ButtonAdd("Murder Mystery 2",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/MurderMystery.lua"))()
     MenuUI:WindowDelete()
 end)
+
+--[[
+'##::::##::':::##':::'########::'########:'########:::::'##::::'##:'##::::'##: ########
+::##::##:::. :::::::: ::.##. ::: ##.....:: ##.... ##:::: ##:::: ##: ##:::: ##: ##.... ##:
+::::##::::::. :##:::: :::##: ::: ##::::::: ##:::: ##:::: ##:::: ##: ##:::: ##: ##:::: ##:
+::##::##:::::. ##:::: :::##::::: ######::: ########::::: #########: ##:::: ##: ########::
+:##....##::::: ##:::: ::.##..::: ##...:::: ##.. ##:::::: ##.... ##: ##:::: ##: ##.... ##:
+:##::::##::::: ##:::: :::##::::: ##::::::: ##::. ##::::: ##:::: ##: ##:::: ##: ##:::: ##:
+:##::::##::::: ##:::: :::##::::: ########: ##:::. ##:::: ##:::: ##:. #######:: ########::
+:::..:::::..:::::..:::.......:::........:::::..:::::..:::::..:::::..:::::::::........::..
+]]--
