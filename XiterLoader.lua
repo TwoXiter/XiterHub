47d6202aef6ef3c230ed50a1c4e52090b11f4373
TwoXiter <xiterdev7@gmail.com>
2024-08-20 01:19:19 -0300

    Update XiterLoader.lua

--- XiterLoader.lua
+++ XiterLoader.lua
@@ -1,18 +1,3 @@
-local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
-MenuUI:WindowCreate("Xiter Hub","","dsc.gg/xiterhub")
-MenuUI:ButtonAdd("Blox Fruits Main",function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/BloxFruits.lua"))()
-    MenuUI:WindowDelete()
-end)
-MenuUI:ButtonAdd("Blox Fruits Kaitun(beta)", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/Kaitun.lua"))()
-    MenuUI:WindowDelete()
-end)
-MenuUI:ButtonAdd("Murder Mystery 2",function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/MurderMystery.lua"))()
-    MenuUI:WindowDelete()
-end)
-
 --[[
 '##::::##::':::##':::'########::'########:'########:::::'##::::'##:'##::::'##: ########
 ::##::##:::. :::::::: ::.##. ::: ##.....:: ##.... ##:::: ##:::: ##: ##:::: ##: ##.... ##:
@@ -23,3 +8,32 @@ end)
 :##::::##::::: ##:::: :::##::::: ########: ##:::. ##:::: ##:::: ##:. #######:: ########::
 :::..:::::..:::::..:::.......:::........:::::..:::::..:::::..:::::..:::::::::........::..
 ]]--
+
+-- Xiter Hub Loading 😈
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/uilib.txt"))()
+-- Ant-Afk ❄️
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+     	game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+		wait(1)
+		game:GetService("VirtualUser"):Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+	end)
+ print("Ant-Afk")
+-- Copy Discord ⛩️
+setclipboard("https://dsc.gg/xiterhub")
+-- List Of Games Script 👾
+local PlaceId = game.PlaceId
+if PlaceId == 142823291 then -- Mm2
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/MurderMystery.lua"))()
+elseif PlaceId == 2753915549 then -- Blox s1
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/BloxFruits.lua"))()
+elseif PlaceId == 4442272183 then -- Blox s2
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/BloxFruits.lua"))()
+elseif PlaceId == 7449423635 then -- Blox s3
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/XiterHub/main/BloxFruits.lua"))()
+end
+-- PlaceId Print
+print(PlaceId)
